dcb6088f16289ac9e947815f5033f43696266fd5
alamgir hossain <alamgir555hossain@gmail.com>
2020-06-28 01:04:48 -0400

    state, props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    person: [
+      {name: 'max', age: '26'},
+      {name: 'menu', age: '27'},
+      {name: 'stepnie', age: '28'}
+    ],
+    other: 'someState'
+  }
+  switchButtonHandler = () => {
+    this.setState({
+      person: [
+        {name: 'maxium', age: '26'},
+        {name: 'menu', age: '27'},
+        {name: 'stepnie', age: '21'}
+      ]
+    })
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <h1>Hi,i am React.</h1>
+       <button onClick={this.switchButtonHandler}>Switch</button>
+       <Person name={this.state.person[0].name} age={this.state.person[1].age} />
+       <Person name={this.state.person[1].name} age={this.state.person[1].age}>My Hobbies: Racing</Person>
+       <Person name={this.state.person[2].name} age={this.state.person[2].age} />
       </div>
     );
   }
